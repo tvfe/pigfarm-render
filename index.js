361ab2610a5500a21574d5f0e27450531b569e28
xosuperpig <zombieyang@tencent.com>
2016-10-10 09:38:26 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var vm = require("vm");
+var extend = require("extend");
+var escape = require("./escape");
+
+var getContentNearError = function (error, templateLines) {
+    var near = '';
+    try {
+        var line = error.stack.split('\n')[1].split(':')[1] - 1;
+        near = '';
+        for (var i = Math.max(0, line - 2); i < Math.min(templateLines.length, line + 3); i++) {
+            near += ((i + 1) + (line == i ? ' >' : '  ') + ' | ') + templateLines[i] + '\n';
+        }
+    } catch (e) {
+    }
+
+    return near;
+};
+
+module.exports = function Renderer(template, helper) {
+    // template helper
+    var helpers = extend(escape.bind(null), helper);
+
+    var vmTemplate = new vm.Script('(data, _)=>{with(data){return `' + template + '`}}', {
+        filename: 'your-template.tpl'
+    });
+    vmTemplate = vmTemplate.runInThisContext();
+    var vmTemplateLines = template.split('\n');
+
+    return function (data) {
+        try {
+            return vmTemplate(data, helpers)
+        } catch (e) {
+            e.wrapper = getContentNearError(e, vmTemplateLines);
+            e.kind = 'render error';
+
+            throw e;
+        }
+    }
+};
